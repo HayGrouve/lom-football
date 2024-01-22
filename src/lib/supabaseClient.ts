8acce6c38230e1c5b916c5ee3ec8cc82fd0016ae
Tsvetomir <haygrouve@gmail.com>
2024-01-22 18:42:43 +0200

    change map src

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://urygpnwnzazqngqhfqri.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeWdwbnduemF6cW5ncWhmcXJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2NDMwNDMsImV4cCI6MTk5MDIxOTA0M30.XSc_yG8bytUoROo1RALNdJReLOZXE31U3yMO8cmecYU"
+  "https://rjydlkxnsngovhyghyuo.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqeWRsa3huc25nb3ZoeWdoeXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5MzQxNzIsImV4cCI6MjAyMTUxMDE3Mn0.hg2dMRNxp6COTKSpAKNVC-WtlA62wiRyW5v4tz25dHk"
 );
